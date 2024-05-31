c8ddcba57b9681796d883ff9cb9e52fef36bd14d
Choton Chandra Bhowmik <83583216+chotonbhowmik@users.noreply.github.com>
2024-06-01 02:44:16 +0600

    footer design done

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,12 +1,14 @@
 
 import { Outlet } from 'react-router-dom';
 import Navbar from '../pages/Navbar/Navbar';
+import Footer from '../components/Home/Footer/Footer';
 
 const Main = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import AboutSection from "../../components/Home/AboutSection/AboutSection";
 import Banner from "../../components/Home/Banner/Banner";
 import FeaturedClass from "../../components/Home/FeaturedClass/FeaturedClass";
 import FeaturedSection from "../../components/Home/FeaturedSection/FeaturedSection";
+
 import ForumSection from "../../components/Home/ForumSection/ForumSection";
 import NewsLetter from "../../components/Home/NewsLetter/NewsLetter";
 import TeamSection from "../../components/Home/TeamSection/TeamSection";
@@ -20,6 +21,7 @@ const Home = () => {
           <ForumSection></ForumSection>
           <NewsLetter></NewsLetter>
           <TeamSection></TeamSection>
+    
         </div>
       </div>
     );
