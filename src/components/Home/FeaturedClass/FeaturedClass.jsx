410e82f650deca317551ced453cf0b8250e8551d
Choton Chandra Bhowmik <83583216+chotonbhowmik@users.noreply.github.com>
2024-06-01 02:38:09 +0600

    team section and newsletter section design done

---
+++ src/components/Home/FeaturedClass/FeaturedClass.jsx
@@ -0,0 +1,181 @@
+import Glide from "@glidejs/glide";
+import { useEffect } from "react";
+
+const FeaturedClass = () => {
+    useEffect(() => {
+      const slider = new Glide(".glide-04", {
+        type: "carousel",
+        focusAt: "center",
+        perView: 3,
+        autoplay: 3500,
+        animationDuration: 700,
+        gap: 24,
+        classNames: {
+          nav: {
+            active: "[&>*]:bg-wuiSlate-700",
+          },
+        },
+        breakpoints: {
+          1024: {
+            perView: 2,
+          },
+          640: {
+            perView: 1,
+          },
+        },
+      }).mount();
+
+      return () => {
+        slider.destroy();
+      };
+    }, []);
+  return (
+    <div>
+      <h1 className="text-center text-3xl font-bold pt-10">
+        Transform Your Fitness Journey with Our Key Features
+      </h1>
+      <p className="text-center text-xl py-5 max-w-3xl mx-auto">
+        Our fitness website offers personalized workout plans, nutrition
+        tracking, and community support to help you achieve your health goals.
+        Explore these features and transform your fitness journey today.
+      </p>
+      <div className="glide-04 relative w-full">
+        {/*    <!-- Slides --> */}
+        <div className="overflow-hidden" data-glide-el="track">
+          <ul className="whitespace-no-wrap flex-no-wrap [backface-visibility: hidden] [transform-style: preserve-3d] [touch-action: pan-Y] [will-change: transform] relative flex w-full overflow-hidden p-0">
+            <li>
+              <div className="overflow-hidden rounded bg-white text-slate-500 shadow-md shadow-slate-200">
+                <div className="p-6">
+                  <h3 className="mb-4 text-xl font-medium text-slate-700">
+                    Something to remember
+                  </h3>
+                  <p>
+                    All components can be copied and pasted and easily
+                    implemented in your tailwind css projects. You can choose
+                    which language you want to copy the desired component and
+                    just hover and click on the component you need and paste it
+                    on your project.
+                  </p>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="overflow-hidden rounded bg-white text-slate-500 shadow-md shadow-slate-200">
+                <div className="p-6">
+                  <h3 className="mb-4 text-xl font-medium text-slate-700">
+                    Something to remember
+                  </h3>
+                  <p>
+                    All components can be copied and pasted and easily
+                    implemented in your tailwind css projects. You can choose
+                    which language you want to copy the desired component and
+                    just hover and click on the component you need and paste it
+                    on your project.
+                  </p>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="overflow-hidden rounded bg-white text-slate-500 shadow-md shadow-slate-200">
+                <div className="p-6">
+                  <h3 className="mb-4 text-xl font-medium text-slate-700">
+                    Something to remember
+                  </h3>
+                  <p>
+                    All components can be copied and pasted and easily
+                    implemented in your tailwind css projects. You can choose
+                    which language you want to copy the desired component and
+                    just hover and click on the component you need and paste it
+                    on your project.
+                  </p>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="overflow-hidden rounded bg-white text-slate-500 shadow-md shadow-slate-200">
+                <div className="p-6">
+                  <h3 className="mb-4 text-xl font-medium text-slate-700">
+                    Something to remember
+                  </h3>
+                  <p>
+                    All components can be copied and pasted and easily
+                    implemented in your tailwind css projects. You can choose
+                    which language you want to copy the desired component and
+                    just hover and click on the component you need and paste it
+                    on your project.
+                  </p>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="overflow-hidden rounded bg-white text-slate-500 shadow-md shadow-slate-200">
+                <div className="p-6">
+                  <h3 className="mb-4 text-xl font-medium text-slate-700">
+                    Something to remember
+                  </h3>
+                  <p>
+                    All components can be copied and pasted and easily
+                    implemented in your tailwind css projects. You can choose
+                    which language you want to copy the desired component and
+                    just hover and click on the component you need and paste it
+                    on your project.
+                  </p>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+        {/*    <!-- Controls --> */}
+        <div
+          className="flex w-full items-center justify-center gap-2 p-4"
+          data-glide-el="controls"
+        >
+          <button
+            className="inline-flex h-8 w-8 items-center justify-center rounded-full border border-slate-700 bg-white/20 text-slate-700 transition duration-300 hover:border-slate-900 hover:text-slate-900 focus-visible:outline-none lg:h-12 lg:w-12"
+            data-glide-dir="<"
+            aria-label="prev slide"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="h-5 w-5"
+            >
+              <title>prev slide</title>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
+              />
+            </svg>
+          </button>
+          <button
+            className="inline-flex h-8 w-8 items-center justify-center rounded-full border border-slate-700 bg-white/20 text-slate-700 transition duration-300 hover:border-slate-900 hover:text-slate-900 focus-visible:outline-none lg:h-12 lg:w-12"
+            data-glide-dir=">"
+            aria-label="next slide"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="h-5 w-5"
+            >
+              <title>next slide</title>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedClass;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,10 @@
 import AboutSection from "../../components/Home/AboutSection/AboutSection";
 import Banner from "../../components/Home/Banner/Banner";
+import FeaturedClass from "../../components/Home/FeaturedClass/FeaturedClass";
 import FeaturedSection from "../../components/Home/FeaturedSection/FeaturedSection";
 import ForumSection from "../../components/Home/ForumSection/ForumSection";
+import NewsLetter from "../../components/Home/NewsLetter/NewsLetter";
+import TeamSection from "../../components/Home/TeamSection/TeamSection";
 import Testimonial from "../../components/Home/Testimonial/Testimonial";
 
 
@@ -12,8 +15,11 @@ const Home = () => {
         <div className="max-w-7xl mx-auto">
           <FeaturedSection></FeaturedSection>
           <AboutSection></AboutSection>
+          <FeaturedClass></FeaturedClass>
           <Testimonial></Testimonial>
           <ForumSection></ForumSection>
+          <NewsLetter></NewsLetter>
+          <TeamSection></TeamSection>
         </div>
       </div>
     );
